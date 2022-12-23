c487aad0375f1150327eadac268cf2d9cbc59ee1
Puneeth Rai <puneeth.rai@intel.com>
2022-12-23 13:41:06 +0530

    change to python3

--- openvino/pre-build.sh
+++ openvino/pre-build.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 curl -O https://storage.openvinotoolkit.org/data/test_data/images/banana.jpg
 curl -O https://storage.openvinotoolkit.org/data/test_data/images/car.bmp
 
--- openvino/run.sh
+++ openvino/run.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 OUTPUT_DIR=${1:-${OUTPUT:-"/mount_folder"}}
 
-python classification_sample_async.py -m public\alexnet\FP32\alexnet.xml -i banana.jpg car.bmp -d GPU -p ${OUTPUT_DIR}
+python3 classification_sample_async.py -m public\alexnet\FP32\alexnet.xml -i banana.jpg car.bmp -d GPU -p ${OUTPUT_DIR}
