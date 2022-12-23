10e4339a8e7ea316234b8e26066605d74ec3c50d
Puneeth Rai <puneeth.rai@intel.com>
2022-12-23 13:28:53 +0530

    pre build script

---
+++ openvino/pre-build.sh
@@ -0,0 +1,6 @@
+curl -O https://storage.openvinotoolkit.org/data/test_data/images/banana.jpg
+curl -O https://storage.openvinotoolkit.org/data/test_data/images/car.bmp
+
+pip install -r requirements.txt
+omz_downloader --name alexnet
+omz_converter --name alexnet
--- openvino/run.sh
+++ openvino/run.sh
@@ -1,10 +1,3 @@
 OUTPUT_DIR=${1:-${OUTPUT:-"/mount_folder"}}
 
-#curl -O https://storage.openvinotoolkit.org/data/test_data/images/banana.jpg
-#curl -O https://storage.openvinotoolkit.org/data/test_data/images/car.bmp
-
-pip install -r requirements.txt
-omz_downloader --name alexnet
-omz_converter --name alexnet
-
 python classification_sample_async.py -m public\alexnet\FP32\alexnet.xml -i banana.jpg car.bmp -d GPU -p ${OUTPUT_DIR}
