b1d784d8145542dc42dd851b29a36836e47e142f
Puneeth Rai <puneeth.rai@intel.com>
2022-12-23 15:57:05 +0530

    only FP16 works

--- openvino/classification_sample_async.py
+++ openvino/classification_sample_async.py
@@ -80,8 +80,8 @@ def print_performance_report():
     duration = end_time - start_time
     fps = images_len / duration
     args = parse_args()
-    # Recommended to place performance.txt file to be placed inside FP32 (which ever precision has been used) folder
-    output_path = f'{args.performance}/FP32/'
+    # Recommended to place performance.txt file to be placed inside FP16 folder
+    output_path = f'{args.performance}/FP16/'
     log.info(f"Loaded model to {args.device} in {load_end_time-load_start_time:.2f} seconds.")
     log.info(f"Total time for {counter} frames: {duration:.2f} seconds, fps:{fps:.2f}")
     Path(output_path).mkdir(parents=True, exist_ok=True)
