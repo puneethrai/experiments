49278a6d344cce032c794b89e405dc929d0a2d49
Puneeth Rai <puneeth.rai@intel.com>
2022-12-23 14:22:55 +0530

    update output path

--- openvino/classification_sample_async.py
+++ openvino/classification_sample_async.py
@@ -80,10 +80,11 @@ def print_performance_report():
     duration = end_time - start_time
     fps = images_len / duration
     args = parse_args()
+    output_path = f'{args.performance}/FP16/'
     print(f"Loaded model to {args.device} in {load_end_time-load_start_time:.2f} seconds.")
     print(f"Total time for {counter} frames: {duration:.2f} seconds, fps:{fps:.2f}")
-    Path(args.performance).mkdir(parents=True, exist_ok=True)
-    f = open(f'{args.performance}/FP16/performance.txt', 'w')
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+    f = open(f'{output_path}/performance.txt', 'w')
     f.write(f'Throughput: {fps:.2f} FPS\nLatency: {duration:.2f} s')
     f.close()
 
