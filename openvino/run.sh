b8b765eb4ae9e442829a7e3e460e0305af014586
Puneeth Rai <puneeth.rai@intel.com>
2022-12-23 13:54:42 +0530

    path fix

--- openvino/run.sh
+++ openvino/run.sh
@@ -2,4 +2,4 @@
 
 OUTPUT_DIR=${1:-${OUTPUT:-"/mount_folder"}}
 
-python3 classification_sample_async.py -m public\alexnet\FP32\alexnet.xml -i banana.jpg car.bmp -d GPU -p ${OUTPUT_DIR}
+python3 classification_sample_async.py -m public\\alexnet\\FP32\\alexnet.xml -i banana.jpg car.bmp -d GPU -p ${OUTPUT_DIR}
